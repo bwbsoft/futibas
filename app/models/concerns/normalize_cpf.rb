cf37e199fbf59a3a78861a2053c16f4043304c1c
Otavio Corrêa P. Silva <otaviocorrea@gmail.com>
2024-08-28 16:47:18 -0300

    add normalize to standardize cpf

---
+++ app/models/concerns/normalize_cpf.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+module NormalizeCpf
+  extend ActiveSupport::Concern
+
+  included do
+    before_validation :normalize_cpf
+  end
+
+  def normalize_cpf
+    self.cpf = get_cpf_formatted(cpf)
+  end
+
+  def get_cpf_formatted(cpf)
+    cpf.to_cpf_format
+  end
+end
